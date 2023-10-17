6125666478a7a56e741e7b7662b8de720c96067b
Lucas <lucas.silva1037@gmail.com>
2023-10-17 01:26:02 -0300

    Dup

--- ta2.py
+++ ta2.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-# MANUALMENTE - 'A' o buffer de pulsos
-# A = []  # Inicialize A como uma lista vazia
+# MANUALMENTE - 'buffer' o buffer de pulsos
+# buffer = []  # Inicialize buffer como uma lista vazia
 # print('Digite os valores do BUFFER (pressione Enter após cada valor):')
 # for i in range(32):
 #     valor = int(input(f'Digite o valor do elemento {i + 1}: '))
-#     A.append(valor)
+#     buffer.append(valor)
 # print('BUFFER preenchido')
-# print(A)
+# print(buffer)
 
 # Leia do diretório MATLAB DRIVE
 # PRIL = np.loadtxt("T_PRIL.txt")
 # BUFFER = np.loadtxt("TBUFFER.txt")
 
 # Se necessário manipular o formato do arquivo txt
-# A = np.round(BUFFER)
-# A = A[4:15, 0:14]
-# A = A.flatten()
-# A = A.tolist()
-# A.sort()
+# buffer = np.round(BUFFER)
+# buffer = buffer[4:15, 0:14]
+# buffer = buffer.flatten()
+# buffer = buffer.tolist()
+# buffer.sort()
 
 # RODANDO O PRIL
 
@@ -33,7 +33,7 @@ primeiros_PPI_possiveis = []
 proximos_PPI_condizentes = []
 ameacas_encontradas = []
 
-qtd_pulsos_buffer = len(A)  # Número de pulsos no buffer
+qtd_pulsos_buffer = len(buffer)  # Número de pulsos no buffer
 
 while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
     
@@ -46,12 +46,12 @@ while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
     limite_superior_intervalo_linha_PRIL = PRIL[linha_PRIL, 1]
     janela_linha_PRIL = PRIL[linha_PRIL, 2]
     
-    # Buscas dentro de A com índice i como parâmetro
+    # Buscas dentro de buffer com índice i como parâmetro
     while indice_buffer <= (qtd_pulsos_buffer - 4):
-        # Varrendo A comparando ao PRIL
+        # Varrendo buffer comparando ao PRIL
         for j in range(indice_buffer + 1, qtd_pulsos_buffer - 2):
             # Seja esta a diferença PPI1 ...
-            Dif = A[j] - A[indice_buffer]
+            Dif = buffer[j] - buffer[indice_buffer]
             # PPI1 está no intervalo do PRIL?
             if (limite_inferior_intervalo_linha_PRIL - janela_linha_PRIL <= Dif <= limite_superior_intervalo_linha_PRIL + janela_linha_PRIL):
                 # Se sim, salve-a em um vetor com os TOAs
@@ -62,10 +62,10 @@ while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
         if primeiros_PPI_possiveis:
             # Tem-se em PPIS1 todos os TOAs possíveis desse intervalo
             for p in range(len(primeiros_PPI_possiveis)):
-                # Para cada TOA em PPIS1, buscar-se-á o subsequente em A
+                # Para cada TOA em PPIS1, buscar-se-á o subsequente em buffer
                 for j in range(indice_buffer + 2, qtd_pulsos_buffer - 1):
                     # Seja esta a diferença PPI2 ...
-                    Dif = A[j] - A[indice_buffer]
+                    Dif = buffer[j] - buffer[indice_buffer]
                     # PPI2 está no mesmo intervalo de PPI1 novamente?
                     if (2 * primeiros_PPI_possiveis[p] - janela_linha_PRIL <= Dif <= 2 * primeiros_PPI_possiveis[p] + janela_linha_PRIL):
                         # Se sim, salve-a em um vetor com os TOAs
@@ -80,12 +80,12 @@ while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
                         contador = [0, 0, 0]
                         PPI345 = [0, 0, 0]
                         dev345 = [0, 0, 0]
-                        # Varrendo A para confirmação de PRI
+                        # Varrendo buffer para confirmação de PRI
                         for r in range(3, 6):
                             # Serão necessários 3 rounds de confirmação
                             # Busca por todo o buffer em cada uma delas
                             for j in range(indice_buffer + 3, qtd_pulsos_buffer):
-                                Dif = A[j] - A[indice_buffer]
+                                Dif = buffer[j] - buffer[indice_buffer]
                                 # Se dentro do intervalo e janela
                                 # Utilizei Wdw*0.67 por conta da média
                                 if ((r * np.mean([primeiros_PPI_possiveis[p], proximos_PPI_condizentes[q]]) - janela_linha_PRIL <= Dif)
@@ -109,7 +109,7 @@ while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
                         if sum(np.array(contador) != 0) >= 2:
                             PRI = [primeiros_PPI_possiveis[p], proximos_PPI_condizentes[q]] + PPI345
                             DEV = [(proximos_PPI_condizentes[q] - primeiros_PPI_possiveis[p])] + dev345
-                            TREM = [A[indice_buffer], np.mean([x for x in PRI if x != 0]), np.mean([x for x in DEV if x != 0])]
+                            TREM = [buffer[indice_buffer], np.mean([x for x in PRI if x != 0]), np.mean([x for x in DEV if x != 0])]
                             ameacas_encontradas.append(TREM)
                             if len(ameacas_encontradas) > 1:
                                 # Encontrar índices de PRIs múltiplos
@@ -124,18 +124,18 @@ while linha_PRIL < len(PRIL):  # Enquanto houver linhas na tabela PRIL
                                     ameacas_encontradas.pop()
                             # Apagar TOAs congruentes
                             # Poderia-se definir uma tolerância de X%
-                            # Zerando os valores em A que são próximos o suficiente de TREM
+                            # Zerando os valores em buffer que são próximos o suficiente de TREM
                             indexador = 0
-                            while (TREM[0] + indexador * np.mean([x for x in PRI if x != 0])) <= max(A):
-                                # Calculando a Diferença entre os termos e os elementos de A
-                                Dif = np.abs(np.array(A) - (TREM[0] + indexador * np.mean([x for x in PRI if x != 0])))
+                            while (TREM[0] + indexador * np.mean([x for x in PRI if x != 0])) <= max(buffer):
+                                # Calculando a Diferença entre os termos e os elementos de buffer
+                                Dif = np.abs(np.array(buffer) - (TREM[0] + indexador * np.mean([x for x in PRI if x != 0])))
                                 # Encontrando os índices dos elementos de dentro de (janela_linha_PRIL*0.34)
                                 Ind = np.where(Dif <= janela_linha_PRIL * 0.34)[0]
-                                # Remove os elementos encontrados de A
-                                A = [x for i, x in enumerate(A) if i not in Ind]
+                                # Remove os elementos encontrados de buffer
+                                buffer = [x for i, x in enumerate(buffer) if i not in Ind]
                                 indexador += 1
                             # PRI ok, Salvo e Apagado!
-                            qtd_pulsos_buffer = len(A)
+                            qtd_pulsos_buffer = len(buffer)
                             # Novo tamanho de buffer
                             indice_buffer = 0
                             # Voltando ao i=1 porque apaguei o mesmo
